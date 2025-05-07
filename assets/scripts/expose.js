11ed46169d4ef8b254a22226bdf69b2c87e5a22f
Jonathan Cohen-Wang <jcohenwang@ucsd.edu>
2025-05-07 02:15:17 -0700

    complete unit tests

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -22,7 +22,6 @@ function init() {
     const volume = parseInt(volumeSlider.value);
     audio.volume = volume / 100;
 
-    // Update volume icon
     let iconLevel;
     if (volume === 0) iconLevel = 0;
     else if (volume < 33) iconLevel = 1;
